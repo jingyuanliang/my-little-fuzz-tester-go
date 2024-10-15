cd8c8d04787ac632d7d0acee1f7c70aa87ee7a28
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2024-10-15 16:22:21 -0700

    Add random input generation capability to fuzzer

--- fuzzer.go
+++ fuzzer.go
@@ -1,6 +1,11 @@
 package main
 
-// Fuzzer struct that holds a function
+import (
+	"fmt"
+	"math/rand"
+)
+
+// Fuzzer struct that holds a function to be fuzzed
 type Fuzzer struct {
 	testFunction func(string) (string, error) // The function to be fuzzed
 }
@@ -10,4 +15,26 @@ func NewFuzzer(testFunction func(string) (string, error)) *Fuzzer {
 	return &Fuzzer{
 		testFunction: testFunction,
 	}
+}
+
+// randomString generates a random string of a given length
+func (f *Fuzzer) randomString(length int) string {
+	letters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	s := make([]byte, length)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
+}
+
+// Fuzz generates random inputs, tests the function, and returns the result and any error
+func (f *Fuzzer) Fuzz() (string, error) {
+	input := f.randomString(rand.Intn(20) + 1) // Generate a random string of length 1 to 20
+	result, err := f.testFunction(input)       // Call the function being fuzzed
+	if err != nil {
+		fmt.Printf("Error with input '%s': %v\n", input, err) // Print the error if one occurs
+		return "", err
+	}
+	fmt.Printf("Success with input '%s': %s\n", input, result) // Print success
+	return result, nil
 }
